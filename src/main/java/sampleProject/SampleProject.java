fedad01da5a3899b56b42863f2461f3edcdd6234
Vigneshkumar0407 <kumarvignesh834@gmail.com>
2021-04-09 13:28:53 +0530

    Sampleproject

---
+++ src/main/java/sampleProject/SampleProject.java
@@ -0,0 +1,24 @@
+package sampleProject;
+
+public class SampleProject {
+
+	public void ChennaiAddress() {
+System.out.println("Adyar");
+	}
+	public void ChennaiAddress1() {
+		System.out.println("Adyar");
+
+	}
+	public void ChennaiAddress2() {
+		System.out.println("Adyar");
+
+	}
+	public void ChennaiAddress3() {
+		System.out.println("Adyar");
+
+	}
+	public void ChennaiAddress4() {
+		System.out.println("Adyar");
+
+	}
+}
